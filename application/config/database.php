97ea8676c0226c04b3897fb7d0164ad69865d08b
Carlos Ernesto Sáenz Vega <cesaenzv@hotmail.com>
2013-02-21 21:12:24 -0500

    Base de datos normalizada

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => '127.0.0.1',
-			'database' => 'aat',
+			'database' => 'aatnormalizada',
 			'username' => 'root',
 			'password' => 'root',
 			'charset'  => 'utf8',
