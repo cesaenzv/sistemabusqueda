04e8be71a3e4dc0336b9d29299951158ed1f64dc
Carlos Ernesto Saenz Vega <cesaenzv@hotmail.com>
2013-02-25 09:13:53 -0500

    Arreglo consulta recursos

--- application/models/metadataRepository.php
+++ application/models/metadataRepository.php
@@ -9,12 +9,15 @@ class MetadataRepository {
 		-Funcionalidad:
 			Metodo encargado de obtener el id_metadata_term con respecto al id_europena_term
 		-Variables:			
-			$idTerm->Id que identifica el termino por el cual se realiza la consulta de los pies
+			$idTerm->Id que identifica el termino por el cual se realiza la consulta de los pies, conocido en la BD
+			como term_id en la tabla de europena terms
 		-Retorno:
 			$resources->Arreglo que contiene las id´s de los metadatos relacionados al id del termino termino
 	*/
 	public function get_MetadataId($idTerm){		
-		$metadatas = DB::table('metadata128')->where_id_europeana_term($idTerm)->get('id_metadata_term');
+		$metadatas = DB::table('metadata128 AS m')
+						->join('eurpeanaterms128 AS e','e.id_europena_term','m.id_europena_term')
+						->where('e.term_id','=',$idTerm)->get('id_metadata_term');
 		return $metadatas;
 	}
 }
