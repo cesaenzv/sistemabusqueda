ce1648415e2b6d8aa3078f2c0500769c8d6fc905
Diego <diegojutsu@gmail.com>
2013-03-03 12:46:35 -0500

    gitnignore

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ return array(
 			'host'     => '127.0.0.1',
 			'database' => 'aatnormalizada',
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
