c93f95fb3f919959a036b20c4fde85fc2c226ee9
Diego Armando Castaño <black-lionheart@hotmail.com>
2013-11-07 22:53:30 -0500

    enrutamiento motores de búsqueda

--- application/views/decide.blade.php
+++ application/views/decide.blade.php
@@ -8,8 +8,8 @@
 	<div class="container_12">
 		<div  class="grid_12 selectContainer">
 			<h1 class="landHeading">Selecciona motor de búsqueda</h1>
-			<a href="" class="landing">Búsqueda semántica</a>
-			<a href="" class="landing">Busqueda tradicional</a>
+			<a href="{{url('semantic')}}" class="landing">Búsqueda semántica</a>
+			<a href="{{url('graphic')}}" class="landing">Busqueda tradicional</a>
 		</div>
 	</div>
 </section>
